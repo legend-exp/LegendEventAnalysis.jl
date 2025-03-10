8ecd24dfe34102f8f33122c504a57e0919a1936a
Florian Henkes <florian.henkes@outlook.com>
2025-03-10 20:00:11 +0100

    Explicit typeof statements for type stability

--- src/calibrate_pmts.jl
+++ src/calibrate_pmts.jl
@@ -93,7 +93,7 @@ end
 function _build_muon_evt_cut(data::LegendData, sel::AnyValiditySelection, global_events::AbstractVector{<:NamedTuple}, pmt_events::AbstractVector{<:NamedTuple})
     output_colnames = (:timestamp, :pe_cal, :multiplicity)
     cutout_colnames = (:is_valid_muon, :n_hit_window)
-    colnames_types = Tuple{Bool, Int, Quantity{<:Real}, Quantity{<:Real}, Int}
+    colnames_types = Tuple{Bool, Int, typeof(0.0u"s"), typeof(0.0u"e_au"), Int}
     if isempty(pmt_events)
         return StructArray(fill(NamedTuple{(cutout_colnames..., output_colnames...), colnames_types}([true, 0, NaN*u"s", 0.0u"e_au", 0]), length(global_events)))
     elseif count(.!pmt_events.is_valid_muon) == 0
