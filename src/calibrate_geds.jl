3f415b8a6822e69a81b02a17bf5172b3fda676a8
Florian Henkes <florian.henkes@outlook.com>
2026-05-11 21:13:54 +0200

    Use cal_chdata for is_trig

--- src/calibrate_geds.jl
+++ src/calibrate_geds.jl
@@ -67,7 +67,7 @@ function calibrate_ged_channel_data(data::LegendData, sel::AnyValiditySelection,
 
     # get qc cut flags
     is_physical = cut_is_physical_pf.(cut_output)
-    is_trig = cut_is_trig_pf.(cut_output)
+    is_trig = cut_is_trig_pf.(cal_chdata)
     # physical trigger is a trigger which is has a physical waveform
     is_physical_trig = is_trig .&& is_physical
     # baseline is a non-physical event which is not a trigger --> can either be a baseline or a physical event which is not a trigger
